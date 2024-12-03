a40934003437996f7187eda26f08747216f6a045
CompSciHigh24 <csh-github@compscihigh.org>
2024-12-03 07:28:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-// Step 1: Look at your dependencies in your package.json file
-// List the dependencies you see below
-/*
+// Step 1: Install Express.js via the terminal command:
+// 'npm install express' or the shorthand version 'npm i express'
 
+// Step 2: Import the express module using 'require', name the variable express
 
-*/
+// Step 3: Create an instance of express() and name the variable 'app'
 
-// Step 2: Install express in your shell (not the console) using the command 'npm install express'
+// Step 4: Set up the server using 'app.listen'. 
+// Set the port is set to 3000. 
+// Leave the callback function empty for now.
 
-// Step 3: Notice what changed in your package.json after installing express. Write the change you saw below:
-/*
+// Step 5: Start your server by running the index.js file
+// Notice how the terminal is running but nothing is being console logged. 
+// Stop your server by using the command ctrl + c
 
+// Step 6: Console log "Server is running at http://localhost:3000" inside the callback function. 
+// Restart your server
 
-*/
+// Step 7: Follow the local host URL. 
 
-// Step 4: Import Express into your file below
-
-// Step 5: Initialize Express Application
-// Create an instance of an Express application to set up our server, call it 'app'.
-
-// Step 4: Start the server with app.listen(). Have it log "Server running." to the console.
-
-// Click run, and look for your message in the console.
-
-// Step 6: Define a GET Route
-// Define a route handler for the root URL ("/"). When a GET request is made to "/", respond with 'Hello World'.
-
-// Run your code and visit '/'
-
-// Step 7: Define another GET Route
-// Respond with your first and last name, and visit your new route
