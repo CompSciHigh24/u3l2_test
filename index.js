7f3ee4fc6b6a172e741b3f99e3f565a7717ea511
CompSciHigh24 <csh-github@compscihigh.org>
2024-12-03 04:05:17 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+// Step 1: Look at your dependencies in your package.json file
+// List the dependencies you see below
+/*
+
+
+*/
+
+// Step 2: Install express in your shell (not the console) using the command 'npm install express'
+
+// Step 3: Notice what changed in your package.json after installing express. Write the change you saw below:
+/*
+
+
+*/
+
+// Step 4: Import Express into your file below
+
+// Step 5: Initialize Express Application
+// Create an instance of an Express application to set up our server, call it 'app'.
+
+// Step 4: Start the server with app.listen(). Have it log "Server running." to the console.
+
+// Click run, and look for your message in the console.
+
+// Step 6: Define a GET Route
+// Define a route handler for the root URL ("/"). When a GET request is made to "/", respond with 'Hello World'.
+
+// Run your code and visit '/'
+
+// Step 7: Define another GET Route
+// Respond with your first and last name, and visit your new route
